b43e74abac5c3a88d70a93e1c227383390bb86a6
Mihir Patel <pmihir2001@gmail.com>
2025-05-21 22:26:23 -0400

    minor changes

--- Shopping-Cart/src/components/NavBar.jsx
+++ Shopping-Cart/src/components/NavBar.jsx
@@ -1,14 +1,32 @@
-import { Container, Nav, Navbar as NavbarBs, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar as NavbarBs,
+  Form,
+  Button
+} from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { FaSearch, FaShoppingCart } from 'react-icons/fa';
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function NavBar({cartItemCount}) {
+function NavBar({ cartItemCount }) {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  let handleForm = (e) => {
+    e.preventDefault()
+    if(!query.trim()){
+      return
+    }
+    navigate(`/shop?search=${query}`);
+
+  };
 
   return (
     <>
       <NavbarBs sticky="top" className="navbar-custom">
         <Container>
-        
           <Nav className="me-auto">
             <Nav.Link
               to="/"
@@ -19,7 +37,6 @@ function NavBar({cartItemCount}) {
             </Nav.Link>
           </Nav>
 
-        
           <Nav className="mx-auto navbar-links">
             <Nav.Link to="/" as={NavLink} className="navbar-item">
               Home
@@ -32,19 +49,18 @@ function NavBar({cartItemCount}) {
             </Nav.Link>
           </Nav>
 
-    
-          <Form className="d-flex search-form">
-            <FormControl
+          <Form className="d-flex search-form" onSubmit={handleForm}>
+            <input
               type="search"
               placeholder="Search"
               aria-label="Search"
               className="search-input"
+              onChange={(e) => setQuery(e.target.value)}
             />
             <Button variant="outline-dark" type="submit" className="search-btn">
-              <FaSearch /> 
+              <FaSearch />
             </Button>
           </Form>
-
 
           <Nav className="ms-3">
             <Nav.Link to="/cart" as={NavLink} className="cart-btn">
